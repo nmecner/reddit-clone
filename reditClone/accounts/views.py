f3a7c18ffe6c4343de3893b142330a9391d6fe16
Natalia Mecner <natalia.mecner@gmail.com>
2018-07-01 22:53:38 +0200

    added creating new users functionality

--- reditClone/accounts/views.py
+++ reditClone/accounts/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+        else:
+            return render(request, 'accounts/signup.html', {'error': "Your passwords don't match"})
+
+        return render(request, 'accounts/signup.html')
+    else:
+        return render(request, 'accounts/signup.html')

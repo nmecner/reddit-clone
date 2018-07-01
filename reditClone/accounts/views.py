a57af416a8e90542890a1a55d28bbf22d1760b61
Natalia Mecner <natalia.mecner@gmail.com>
2018-07-01 23:12:35 +0200

    prevent using usernames that already exist

--- reditClone/accounts/views.py
+++ reditClone/accounts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
 
 def signup(request):
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
-            User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': "User like that already exists"})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                login(request, user)
+                return render(request, 'accounts/signup.html')
         else:
             return render(request, 'accounts/signup.html', {'error': "Your passwords don't match"})
-
-        return render(request, 'accounts/signup.html')
     else:
         return render(request, 'accounts/signup.html')
